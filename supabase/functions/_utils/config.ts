9cdbc95aa662b653ec9260c6b3626315d3092cd0
William Shepherd <williamrshepherd@gmail.com>
2022-11-23 12:11:07 -0600

    Add new environment variables

---
+++ supabase/functions/_utils/config.ts
@@ -0,0 +1,10 @@
+export const _SupabaseUrl_ = Deno.env.get("SUPABASE_URL") ?? ""
+export const _SupabaseServiceRoleKey_ =
+  Deno.env.get("SUPABASE_SERVICE_ROLE_KEY") ?? ""
+export const _StripePublishableKey_ =
+  Deno.env.get("STRIPE_PUBLISHABLE_KEY") ?? ""
+
+export const _StripeSecretKey_ = Deno.env.get("STRIPE_SECRET_KEY") ?? ""
+
+// For debugging environment
+// console.log("Runtime environmentDeno\n\t", Deno.env.toObject())
