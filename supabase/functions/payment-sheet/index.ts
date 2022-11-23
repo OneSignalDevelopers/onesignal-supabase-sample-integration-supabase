79f484fe7af20fd4dd2afcf685a5e2a331d80cc7
William Shepherd <williamrshepherd@gmail.com>
2022-11-23 12:10:17 -0600

    Use better errors
    Also includes some minor cleanup

--- supabase/functions/payment-sheet/index.ts
+++ supabase/functions/payment-sheet/index.ts
@@ -1,54 +1,59 @@
 import { serve } from "https://deno.land/std@0.165.0/http/server.ts"
-import { stripe } from "../_utils/stripe.ts"
+import Stripe from "https://esm.sh/stripe@10.13.0?target=deno&deno-std=0.165.0"
+
+import { _StripePublishableKey_, _StripeSecretKey_ } from "../_utils/config.ts"
 import { createOrRetrieveCustomer } from "../_utils/supabase.ts"
+
+const stripe = Stripe(_StripeSecretKey_, {
+  httpClient: Stripe.createFetchHttpClient(),
+})
 
 console.log("payment-sheet handler up and running!")
 
 serve(async (req) => {
-  // return new Response(JSON.stringify(req), {
-  //   headers: { "Content-Type": "application/json" },
-  //   status: 200,
-  // })
-
   try {
     // Get the authorization header from the request.
     // When you invoke the function via the client library it will automatically pass the authenticated user's JWT.
-    const authHeader = req.headers.get("Authorization")!
-    console.log("authHeader", authHeader)
+    const jwt = req.headers.get("Authorization")!
+    console.log(`User JWT:\n\t`, jwt)
     // Retrieve the logged in user's Stripe customer ID or create a new customer object for them.
     // See ../_utils/supabase.ts for the implementation.
-    const customer = await createOrRetrieveCustomer(authHeader)
-    console.log("customer", customer)
+    const customer = await createOrRetrieveCustomer(jwt)
+    console.log(`Customer Id:\n\t`, customer)
+
     // Create an ephermeralKey so that the Stripe SDK can fetch the customer's stored payment methods.
     const ephemeralKey = await stripe.ephemeralKeys.create(
-      { customer: customer },
+      {
+        customer: customer,
+      },
       { apiVersion: "2020-08-27" }
     )
-    console.log("ephemeralKey", ephemeralKey)
+    console.log(`Stripe ephemeralKey:\n\t`, ephemeralKey)
+
     // Create a PaymentIntent so that the SDK can charge the logged in customer.
     const paymentIntent = await stripe.paymentIntents.create({
       amount: 1099,
       currency: "usd",
       customer: customer,
     })
-    console.log("paymentIntent", paymentIntent)
+    console.log(`Stripe paymentIntent:\n\t`, paymentIntent)
+
     // Return the customer details as well as teh Stripe publishable key which we have set in our secrets.
     const res = {
-      stripe_pk: Deno.env.get("STRIPE_PUBLISHABLE_KEY"),
+      stripe_pk: _StripePublishableKey_,
       paymentIntent: paymentIntent.client_secret,
       ephemeralKey: ephemeralKey.secret,
       customer: customer,
     }
-    console.log("res", res)
-    return new Response(JSON.stringify(res), {
+    console.log(`Stripe response:\n\t`, res)
+
+    return new Response(JSON.stringify({}), {
       headers: { "Content-Type": "application/json" },
       status: 200,
     })
   } catch (error) {
-    console.log("err", error)
-
-    const err = JSON.stringify(error)
-    return new Response(err, {
+    console.error("Payment sheet error ", error)
+    return new Response(JSON.stringify(error), {
       headers: { "Content-Type": "application/json" },
       status: 400,
     })
