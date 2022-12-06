e79c3c2dd144e8a5f70ff12ff677c20b4d727454
William Shepherd <williamrshepherd@gmail.com>
2022-12-06 11:47:47 -0600

    Add new endpoint to send email
    This will be use in situations where the user has not opted-into push notifications

---
+++ supabase/functions/email-order-confirmation-to-customer/index.ts
@@ -0,0 +1,43 @@
+import { serve } from "https://deno.land/std@0.131.0/http/server.ts"
+import * as OneSignal from "https://esm.sh/@onesignal/node-onesignal@1.0.0-beta7"
+import { onesignal, _OnesignalAppId_ } from "../_utils/config.ts"
+import { getCustomerProfile } from "../_utils/supabase.ts"
+
+const generateEmail = (amount: number, currency: string) =>
+  `<html><body>You just spent ${amount / 100} ${(
+    currency as String
+  ).toUpperCase()}.</body></html>`
+
+serve(async (req) => {
+  try {
+    const { record } = await req.json()
+    const customerId = record.stripe_customer_id
+    const profile: string | null = await getCustomerProfile(customerId)
+    if (!profile) {
+      throw Error(`No profile found for Stripe customer ${customerId}.`)
+    }
+
+    // Create message object
+    const message: OneSignal.Notification = {
+      app_id: _OnesignalAppId_,
+      include_external_user_ids: [profile],
+      email_subject: "You order confirmation",
+      email_body: generateEmail(record.amount, record.currency),
+    }
+
+    const onesignalApiRes = await onesignal.createNotification(message)
+
+    return new Response(
+      JSON.stringify({ onesignalResponse: onesignalApiRes }),
+      {
+        headers: { "Content-Type": "application/json" },
+      }
+    )
+  } catch (err) {
+    console.error("Failed to create OneSignal notification", err)
+    return new Response("Server error.", {
+      headers: { "Content-Type": "application/json" },
+      status: 400,
+    })
+  }
+})
